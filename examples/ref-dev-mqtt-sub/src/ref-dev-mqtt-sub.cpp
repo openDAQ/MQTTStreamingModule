8bcaba9d579a3165d0c3bf604b266557dd960223
Viacheslau <viacheslau.kalenikau@gmail.com>
2025-11-05 11:17:46 +0100

    mqtt: fixes for the ref-dev-mqtt-sub example

--- examples/ref-dev-mqtt-sub/src/ref-dev-mqtt-sub.cpp
+++ examples/ref-dev-mqtt-sub/src/ref-dev-mqtt-sub.cpp
@@ -7,6 +7,36 @@
 using namespace daq;
 using namespace daq::modules::mqtt_streaming_client_module;
 
+std::string to_string(daq::DataPacketPtr packet)
+{
+    std::string result;
+    std::string data;
+    switch (packet.getDataDescriptor().getSampleType())
+    {
+    case SampleType::Float64:
+        data = std::to_string(*(static_cast<double*>(packet.getData())));
+        break;
+    case SampleType::UInt64:
+        data = std::to_string(*(static_cast<uint64_t*>(packet.getData())));
+        break;
+    case SampleType::Int64:
+        data = std::to_string(*(static_cast<int64_t*>(packet.getData())));
+        break;
+    case SampleType::Binary:
+        data = '\"' + std::string(static_cast<char*>(packet.getData()), packet.getDataSize()) + '\"';
+        break;
+    default:
+        break;
+    }
+    result = fmt::format("Data: {};", data);
+    if (packet.getDomainPacket().assigned())
+    {
+        uint64_t ts = *(static_cast<uint64_t*>(packet.getDomainPacket().getData()));
+        result += fmt::format(" Timestamp : {};", ts);
+    }
+    return result;
+}
+
 int main(int argc, char* argv[])
 {
     InputArgs args;
@@ -40,29 +70,34 @@ int main(int argc, char* argv[])
     std::cout << "Try to connect the " << fbList[0].getLocalId() << std::endl;
 
     auto signals = fbList[0].getSignals();
-    std::vector<StreamReaderPtr> readers;
+    std::vector<PacketReaderPtr> readers;
     for (const auto& s : signals) {
-        readers.emplace_back(daq::StreamReader<double, uint64_t>(s, ReadTimeoutType::Any));
+        readers.emplace_back(daq::PacketReader(s));
     }
 
-    std::thread t1([readers]() {
-        constexpr int size = 1000;
-        double samples[size];
-        uint64_t timestamps[size];
+    std::thread readerThread([readers]() {
         while (true) {
             for (int iRdr = 0; iRdr < readers.size(); ++iRdr) {
                 const auto& reader = readers[iRdr];
                 while (!reader.getEmpty()) {
-                    daq::SizeT count = size;
-                    reader.readWithDomain(samples, timestamps, &count);
-                    for (daq::SizeT i = 0; i < count; ++i)
-                        std::cout << "READER #" << iRdr << " - Sample: " << samples[i] << " Timestamp: " << timestamps[i] << std::endl;
+                    auto packet = reader.read();
+                    if (packet.getType() == PacketType::Event)
+                    {
+                        std::cout << "Event packet is skipped!" << std::endl;
+                        continue;
+                    }
+
+                    if (packet.getType() == PacketType::Data)
+                    {
+                        const auto dataPacket = packet.asPtrOrNull<IDataPacket>();
+                        std::cout << "READER #" << iRdr << " - " << to_string(dataPacket) << std::endl;
+                    }
                 }
             }
             std::this_thread::sleep_for(std::chrono::milliseconds(20));
         }
     });
-    t1.detach();
+    readerThread.detach();
 
     std::cout << "Press \"enter\" to exit the application..." << std::endl;
     std::cin.get();
