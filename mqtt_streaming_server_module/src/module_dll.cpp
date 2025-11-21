009a4c8221bd2e5fa538bcc210f6e885e212fd0a
Viacheslau <viacheslau.kalenikau@gmail.com>
2025-11-21 15:21:36 +0100

    mqtt: demo

--- mqtt_streaming_server_module/src/module_dll.cpp
+++
@@ -1,9 +0,0 @@
-#include <mqtt_streaming_server_module/module_dll.h>
-#include <mqtt_streaming_server_module/mqtt_streaming_server_module_impl.h>
-
-#include <opendaq/module_factory.h>
-
-using namespace daq::modules::mqtt_streaming_server_module;
-
-DEFINE_MODULE_EXPORTS(MqttStreamingServerModule)
-
